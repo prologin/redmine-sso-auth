b31ef5e5423503101693dea20e3a5d250642078d
Alexandre `Zopieux` Macabies <web+git@zopieux.com>
2015-04-25 14:08:24 +0200

    Rename and refactor a bunch of stuff so Redmine/Rails class loader is happy

--- lib/redmine_sso_auth/sso_auth_patch.rb
+++ lib/redmine_sso_auth/sso_auth_patch.rb
@@ -1,21 +1,29 @@
 module RedmineSSOAuth
   module SSOAuthPatch
     unloadable
-    
+
     def self.included(base)
       base.send(:include, ClassMethods)
       base.class_eval do
         alias_method_chain(:find_current_user, :ssoauth)
       end
     end
-  
+
     module ClassMethods
-      include SSOAuthHelper
-  
+
+      def use_email?
+        Setting.plugin_redmine_sso_auth['lookup_mode'] == 'mail'
+      end
+
+      def remote_user
+        Rails.logger.debug 'attempting to build remote-user'
+        request.env[Setting.plugin_redmine_sso_auth['server_env_var']]
+      end
+
       def find_current_user_with_ssoauth
         user = find_current_user_without_ssoauth
         return user unless Setting.plugin_redmine_sso_auth['enable'] == "true"
-  
+
         remote_username = remote_user
         if remote_username.nil?
           if !used_sso_authentication? || Setting.plugin_redmine_sso_auth['keep_sessions'] == "true"
@@ -24,13 +32,13 @@ module RedmineSSOAuth
           reset_session
           return nil
         end
-  
+
         return user unless session_changed? user, remote_username
-  
+
         reset_session
         try_login remote_username
       end
-  
+
       def try_login(remote_username)
         if use_email?
           user = User.active.find_by_mail remote_username
@@ -44,15 +52,15 @@ module RedmineSSOAuth
           do_login user
         end
       end
-  
+
       def used_sso_authentication?
         session[:sso_authentication] == true
       end
-  
+
       def use_email?
         Setting.plugin_redmine_sso_auth['lookup_mode'] == 'mail'
       end
-  
+
       def session_changed?(user, remote_username)
         if user.nil?
           true
@@ -60,7 +68,7 @@ module RedmineSSOAuth
           use_email? ? user.mail.casecmp(remote_username) != 0 : user.login.casecmp(remote_username) != 0
         end
       end
-  
+
       def do_login(user)
         if (user && user.is_a?(User))
           start_user_session(user)
